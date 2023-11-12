cbb7a002dd5cbe85220135c8e4efd633cfd6a4e0
Shah Md. Imran Hossain <akashganga.programming.534363@gmail.com>
2023-11-12 23:48:34 +0600

    project init

---
+++ SimpleMathGame/SimpleMathGameApp.swift
@@ -0,0 +1,17 @@
+//
+//  SimpleMathGameApp.swift
+//  SimpleMathGame
+//
+//  Created by Shah Md Imran Hossain on 12/11/23.
+//
+
+import SwiftUI
+
+@main
+struct SimpleMathGameApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
